1e629ff9046405d6a3d0275156fbc5a5f5bda80f
Javier Salas <javier.salas.991@gmail.com>
2021-11-24 19:58:53 -0300

    guardo

--- src/components/inicio/Novedades.js
+++ src/components/inicio/Novedades.js
@@ -6,6 +6,9 @@ const Novedades = () => {
         <div className="d-flex justify-content-center" style={{'font-size': '41px'}}>
           <p className="text-light">Novedades</p>
         </div>
+        <div style={{'background-color': 'rgb(29, 33, 60)', 'margin' : '0 5%'}}>
+            <div className='cuadro-blanco'></div>
+        </div>
     </div>
   );
 };
