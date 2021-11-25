e1ea9c98a837a98934dc40285f4974925ab5d230
Javier Salas <javier.salas.991@gmail.com>
2021-11-25 20:09:25 -0300

    guardo

--- src/components/inicio/Novedades.js
+++ src/components/inicio/Novedades.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import imagenPrueba from "../../img/cursos/curso1.png";
+import { iconos } from "../helpers/iconos";
 
 const Novedades = () => {
+  const [categoriaSeleccionada, setCategoriaSeleccionada] = useState("Cursos");
+
+  let categorias = ["Cursos", "Convocatorias", "FAQs"];
   return (
     <div id="background-azul-oscuro" className="pt-3 pb-2">
       <div
@@ -27,9 +31,50 @@ const Novedades = () => {
           </div>
         </div>
 
-        <div className="col-sm-12 col-md-6 bg-secondary">
-          <div className="d-flex justify-content-around container">
-            <p className="categoria-seleccionada cursor-pointer">Cursos</p>
+        {/* ///////////////////////// */}
+        <div className="col-sm-12 col-md-6 contenedor-novedades d-flex justify-content-center align-items-center">
+          <div className="contenedor-novedades-derecho">
+            <div className="d-flex justify-content-around px-4">
+              {categorias.map((categoria, key) => {
+                if (categoriaSeleccionada === categoria) {
+                  return (
+                    <>
+                      {key > 0 ? <div className="lineaCeleste"></div> : null}
+                      <p className="categoria colorsito-celeste cursor-pointer">
+                        {categoria
+                          .replace("_", " ")
+                          .replace("_", " ")
+                          .replace(/\w\S*/g, (w) =>
+                            w.replace(/^\w/, (c) => c.toUpperCase())
+                          )}
+                      </p>
+                    </>
+                  );
+                } else {
+                  return (
+                    <>
+                      {key > 0 ? <div className="lineaCeleste"></div> : null}
+                      <p
+                        onClick={() => setCategoriaSeleccionada(categoria)}
+                        className="categoria cursor-pointer"
+                      >
+                        {categoria
+                          .replace("_", " ")
+                          .replace("_", " ")
+                          .replace(/\w\S*/g, (w) =>
+                            w.replace(/^\w/, (c) => c.toUpperCase())
+                          )}
+                      </p>
+                    </>
+                  );
+                }
+              })}
+            </div>
+            <div classname="d-flex">
+              <p className="titulo-seccion-novedades">Recursos humanos</p>
+              <p className='colorsito-amarillo'>{iconos.estrella}{iconos.estrella}{iconos.estrella}</p>
+              <p className='colorsito-amarillo'>{iconos.estrella}{iconos.estrella}{iconos.estrella}</p>
+            </div>
           </div>
         </div>
       </div>
